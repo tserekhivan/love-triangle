3030bc4203a80d6e9bafa5a748d9c921adf74156
Tserekh Ivan <tserekh.ivan@gmail.com>
2018-09-11 16:34:39 +0300

    11 Sep 2018

--- src/index.js
+++ src/index.js
@@ -4,4 +4,29 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var count = 0;
+  var membersOfTriangles = [];
+  var k, l;
+
+  for (let i = 0; i < preferences.length; i++) {
+    if (isNotMemberOfTriangle(membersOfTriangles, i)) {
+      k = preferences[i]-1;
+      if ((i+1) != preferences[k]) {
+        l = preferences[k]-1;
+        if (preferences[l] == (i+1)) {
+          count ++;
+          membersOfTriangles.push (i, k, l);
+        }
+      }
+    }
+  }
+  return count;
+};
+
+function isNotMemberOfTriangle (membersOfTriangles = [], numMember){
+  for (let i=0; i<membersOfTriangles.length; i++) {
+    
+      if (membersOfTriangles[i] == numMember) return false;
+  }
+  return true;
 };
